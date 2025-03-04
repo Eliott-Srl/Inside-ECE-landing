0cd0cbba36f06103266afd520e9de936467c2563
eliott <contact@eliottsrl.fr>
2025-03-04 19:44:38 +0100

    Passage d'un page à l'autre

--- src/animations.js
+++ src/animations.js
@@ -6,9 +6,58 @@ import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-document.addEventListener("DOMContentLoaded", function () {
+const style = window.getComputedStyle(document.body)
 
+let val_entrer = parseFloat(style.getPropertyValue('--font-size')) * (parseFloat(style.getPropertyValue('--pad-1')) + parseFloat(style.getPropertyValue('--pad-2')))
+
+const tl_rentrer = gsap.timeline({
+    scrollTrigger: {
+        markers: false,
+        trigger: "#bento-ctf",
+        start: "top 50%",
+        end: "top " + val_entrer.toString(),
+        scrub: true
+    }
 })
+
+tl_rentrer.to("html", {
+    "--c-b-top": style.getPropertyValue('--pad-1-inverse'),
+    "--c-b-left": style.getPropertyValue('--pad-1-inverse'),
+    "--c-b-width": style.getPropertyValue('--pad-1'),
+    "--c-b-height": style.getPropertyValue('--pad-1'),
+    "--c-b-border-radius": 0
+}, 0)
+
+gsap.to("#hero", {
+    scrollTrigger: {
+        trigger: "#metiers",
+        markers: false,
+        start: "top 50%",
+        end: "top 50%",
+        toggleActions: "play none none reset"
+    },
+    display: "none",
+    duration: 0.01
+})
+
+const tl_sortie = gsap.timeline({
+    scrollTrigger: {
+        markers: false,
+        trigger: "#metiers",
+        start: "bottom 90%",
+        end: "bottom 50%",
+        scrub: true
+    }
+})
+
+tl_sortie.to("html", {
+    "--c-b-top": 0,
+    "--c-b-left": 0,
+    "--c-b-width": 0,
+    "--c-b-height": 0,
+    "--c-b-border-radius": style.getPropertyValue("--bor-rad-1")
+}, 0)
+
 
 const lenis = new Lenis();
 
