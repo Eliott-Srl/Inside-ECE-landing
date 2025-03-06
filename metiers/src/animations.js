8090fef6de66d87d79b880be05bfe05d256be995
Eliott SRL <eliottsorel33@gmail.com>
2025-03-06 02:35:01 +0100

    New page: Jobs

---
+++ metiers/src/animations.js
@@ -0,0 +1,30 @@
+import Lenis from 'lenis'
+import "lenis/dist/lenis.css"
+
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#hero", {
+    scrollTrigger: {
+        trigger: ".page-content",
+        markers: false,
+        start: "50% 50%",
+        end: "50% 50%",
+        toggleActions: "play none none reset"
+    },
+    opacity: 0,
+    zIndex: -2,
+    duration: 0.01
+})
+
+
+const lenis = new Lenis();
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
