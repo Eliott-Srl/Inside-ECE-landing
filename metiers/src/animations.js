b0b052a831307a0601306fb1a6a0d9baf63135d7
eliott <contact@eliottsrl.fr>
2025-03-06 18:53:56 +0100

    TKT

--- metiers/src/animations.js
+++ metiers/src/animations.js
@@ -6,6 +6,8 @@ import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+const style = window.getComputedStyle(document.body);
+
 gsap.to("#hero", {
     scrollTrigger: {
         trigger: ".page-content",
@@ -19,12 +21,25 @@ gsap.to("#hero", {
     duration: 0.01
 })
 
-
 const lenis = new Lenis();
 
 function raf(time) {
     lenis.raf(time);
     requestAnimationFrame(raf);
 }
+
+// for (let button in document.getElementsByClassName("button-select")) {
+//     button.addEventListener("click", (event) => {
+//         if (!button.classList.contains("button-select")) {
+//             button.
+//         }
+//     })
+// }
+
+// window.onresize = (event) => {
+//     console.log("--top-spacing: px;");
+//     document.documentElement.style.cssText = "--top-spacing: " + document.getElementById('hero').offsetHeight.toString() + "px;";
+// }
+// console.log("--top-spacing: px;");
 
 requestAnimationFrame(raf);
